9b596eb5d17efd74ae7e2c7eb37b2057b1d069d2
Kazushige TAKEUCHI <kazushige.takeuchi@gmail.com>
2013-12-07 20:23:37 +0900

    GPSとかINTERNETのパーミッションを付与。Java Clientライブラリは使えない旨聞いてなくて、3歩戻る。

--- client/EyeCatch/src/io/github/oho_sugu/eyecatch/textrecognition/TextRecognitionClient.java
+++ client/EyeCatch/src/io/github/oho_sugu/eyecatch/textrecognition/TextRecognitionClient.java
@@ -2,7 +2,23 @@ package io.github.oho_sugu.eyecatch.textrecognition;
 
 import io.github.oho_sugu.eyecatch.EyeCatchActivity;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.concurrent.Semaphore;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MIME;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.ByteArrayBody;
+import org.apache.http.entity.mime.content.FileBody;
+
+import com.fasterxml.jackson.core.util.ByteArrayBuilder;
 
 import jp.ne.nttdocomo.spm.api.common.exception.SdkException;
 import jp.ne.nttdocomo.spm.api.common.exception.ServerException;
@@ -17,11 +33,13 @@ import jp.ne.nttdocomo.spm.api.recognition.param.CharacterRecognizeJobInfoReques
 import jp.ne.nttdocomo.spm.api.recognition.param.CharacterRecognizeRequestParam;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
+import android.net.http.AndroidHttpClient;
+import android.os.Environment;
 import android.util.Log;
 
 public class TextRecognitionClient {
-	private boolean mIsRequesting = false;
-	private Semaphore mRequestSemaphore;
+	private  boolean mIsRequesting = false;
+	private static Semaphore mRequestSemaphore;
 	CharacterRecognize mRecognize;
 	private String TAG = EyeCatchActivity.TAG;
 
@@ -34,11 +52,11 @@ public class TextRecognitionClient {
 
 	}
 
-	synchronized public boolean canRequest() {
+	synchronized public static boolean canRequest() {
 		boolean isRequesting = false;
 		int availableCount = 0;
 		availableCount = mRequestSemaphore.availablePermits();
-		Log.d(TAG, "canRequest");
+//		Log.d(TAG, "canRequest");
 		if (availableCount == 0) {
 			return false;
 		} else {
@@ -55,10 +73,36 @@ public class TextRecognitionClient {
 			mRequestSemaphore.acquire();
 
 			Log.d(TAG, "Requesting.");
+			byte[] jpegImage = convertToJpeg(format, imageData);
+
+			try {
+				AndroidHttpClient client = AndroidHttpClient
+						.newInstance("Android UserAgent");
+				HttpPost request = new HttpPost(
+						"https://api.apigw.smt.docomo.ne.jp/characterRecognition/v1/scene?APIKEY=74545a3739775362454c313463572f36783932334571562f6338753432775633394b6a374b4a754f7a3541");
+				MultipartEntity multipartEntity = new MultipartEntity(
+						HttpMultipartMode.BROWSER_COMPATIBLE);
+				ByteArrayBody imageBody = new ByteArrayBody(jpegImage, "image/jpeg", "recognition.jpg");
+				multipartEntity.addPart("image", imageBody);
+				request.setEntity(multipartEntity);
+				HttpResponse res;
+				res = client.execute(request);
+				// HttpResponseのEntityデータをStringへ変換
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(res.getEntity().getContent(),
+								"UTF-8"));
+				String jsonstr = reader.toString();
+				Log.d(EyeCatchActivity.TAG, jsonstr);
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				Log.e(EyeCatchActivity.TAG, e.toString());
+			}
 
 			CharacterRecognizeRequestParam requestParam = new CharacterRecognizeRequestParam();
 			requestParam.setLang(Lang.CHARACTERS_JP);
-			// requestParam.setImageData(“image.jpgのフルパス”);
+			requestParam.setImageData(jpegImage);
 			requestParam.setImageContentType(ImageContentType.IMAGE_JPEG);
 			// 認識処理クラスにリクエストデータを渡し、レスポンスデータを取得する
 			try {
@@ -89,14 +133,21 @@ public class TextRecognitionClient {
 		return ret;
 	}
 
-	private byte[] convertToJpeg(int format ,byte[] yuvImage){
-		byte[] jpegImage =null;
+	private byte[] convertToJpeg(int format, byte[] yuvImage) {
+		byte[] jpegImage = null;
 		YuvImage image = new YuvImage(yuvImage, format, 428, 240, null);
 
 		Rect rect = new Rect(0, 0, 428, 240);
-
+		try {
+			ByteArrayOutputStream output = new ByteArrayOutputStream();
+			image.compressToJpeg(rect, 100, output);
+			jpegImage = output.toByteArray();
+			output.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return jpegImage;
-		
-		
+
 	}
 }
